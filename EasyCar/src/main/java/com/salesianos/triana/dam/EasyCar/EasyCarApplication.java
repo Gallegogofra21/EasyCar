58fb51572282ce34e902c86a11d99d45ab537f5b
gallego.gofra21 <gallego.gofra21@triana.salesianos.edu>
2022-04-08 13:41:16 +0200

    Añadido modelo de datos en UML

--- EasyCar/src/main/java/com/salesianos/triana/dam/EasyCar/EasyCarApplication.java
+++ EasyCar/src/main/java/com/salesianos/triana/dam/EasyCar/EasyCarApplication.java
@@ -2,8 +2,11 @@ package com.salesianos.triana.dam.EasyCar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+
 public class EasyCarApplication {
 
 	public static void main(String[] args) {
