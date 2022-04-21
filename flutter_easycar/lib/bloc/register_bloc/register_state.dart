3256ac58eeb3dded0d64266858b7c173319c8009
Fran <gallego.gofra21@triana.salesianos.edu>
2022-04-21 09:44:32 +0200

    Proyecto base y avances

---
+++ flutter_easycar/lib/bloc/register_bloc/register_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_easycar/models/user.dart';
+
+abstract class RegisterState extends Equatable {
+  const RegisterState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class RegisterInitial extends RegisterState {}
+
+class RegisterLoading extends RegisterState {}
+
+class RegisterSuccessState extends RegisterState {
+  final User registerResponse;
+
+  const RegisterSuccessState(this.registerResponse);
+
+  @override
+  List<Object> get props => [registerResponse];
+}
+
+class LoginErrorState extends RegisterState {
+  final String message;
+
+  const LoginErrorState(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
