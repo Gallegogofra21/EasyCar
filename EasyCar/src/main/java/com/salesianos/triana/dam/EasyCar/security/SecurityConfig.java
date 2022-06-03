b7f785a4bb08d7a691deab39d9ac3db529c59f5a
gallego.gofra21 <gallego.gofra21@triana.salesianos.edu>
2022-06-03 12:21:33 +0200

    Avances para despliegue + cors en security

--- EasyCar/src/main/java/com/salesianos/triana/dam/EasyCar/security/SecurityConfig.java
+++ EasyCar/src/main/java/com/salesianos/triana/dam/EasyCar/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //http.cors().configurationSource(corsConfigurationSourcer());
         http
                 .csrf().disable()
                 .exceptionHandling()
